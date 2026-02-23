858c011e32a62dfe4e79bb10a34f442e85060a2b
oscar <oscar.burman005@gmail.com>
2026-02-23 21:01:59 +0100

    Auth mock controllers

--- auth/src/auth/auth.controller.ts
+++ auth/src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post("signup")
+  signup(){
+    return "sign up route"
+  }
+
+  @Post("signin")
+  signin(){
+    return "sign in route"
+  }
+
+  @Get("signout")
+  signout(){
+    return "sign out route"
+  }
 }
