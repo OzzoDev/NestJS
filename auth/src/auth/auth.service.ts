a409da191be50214705ff074a192b5cb24710349
oscar <oscar.burman005@gmail.com>
2026-02-24 21:54:32 +0100

    User model

--- auth/src/auth/auth.service.ts
+++ auth/src/auth/auth.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-    constructor(){}
+  constructor(private prisma: PrismaService) {}
 
-    async signup(){
-        return {message:"signup was successful"}
-    }
+  async signup() {
+    return { message: 'signup was successful' };
+  }
 
-    async signin(){
-        return ""
-    }
+  async signin() {
+    return '';
+  }
 
-    async signout(){
-        return ""
-    }
+  async signout() {
+    return '';
+  }
 }
