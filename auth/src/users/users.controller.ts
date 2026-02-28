65b9be2b508d6c917ac4e152e7fe80202fe2ad51
oscar <oscar.burman005@gmail.com>
2026-02-28 15:25:43 +0100

    Verify auth guard

--- auth/src/users/users.controller.ts
+++ auth/src/users/users.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   getMyUser(@Param() params: { id: string }) {
     const { id } = params;
